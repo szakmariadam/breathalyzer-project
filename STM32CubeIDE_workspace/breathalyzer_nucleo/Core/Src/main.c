828a7da9b624b1e1c22fbcae3d88bc3df3f77f5b
Szakmariadam <Szakmari.adam@gmail.com>
2025-04-02 19:25:45 +0200

    ADC moved to PA1
    and sysWakeup was initialized on pa0

--- STM32CubeIDE_workspace/breathalyzer_nucleo/Core/Src/main.c
+++ STM32CubeIDE_workspace/breathalyzer_nucleo/Core/Src/main.c
@@ -275,7 +275,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
